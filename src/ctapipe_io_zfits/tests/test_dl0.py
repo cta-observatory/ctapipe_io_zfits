3c3bba4dad801c282390979086dd6316c8d8474f
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-08-30 16:34:54 +0200

    Add test using EventSource

--- src/ctapipe_io_zfits/tests/test_dl0.py
+++ src/ctapipe_io_zfits/tests/test_dl0.py
@@ -1,4 +1,13 @@
+from ctapipe.io import EventSource
+
+
 def test_is_compatible(dummy_dl0):
     from ctapipe_io_zfits import ProtozfitsDL0EventSource
 
     assert ProtozfitsDL0EventSource.is_compatible(dummy_dl0)
+
+
+def test_is_valid_eventsource(dummy_dl0):
+    from ctapipe_io_zfits.dl0 import ProtozfitsDL0EventSource
+
+    assert isinstance(EventSource(dummy_dl0), ProtozfitsDL0EventSource)
